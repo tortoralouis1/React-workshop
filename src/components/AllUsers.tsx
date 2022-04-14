86aaf8028729205b4abff240e1aff9b37b9c14b7
tortoralouis1 <tortora.louis@gmail.com>
2022-04-14 11:32:31 +0200

    afficher les users

--- src/components/AllUsers.tsx
+++ src/components/AllUsers.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import { getAllUser } from "../api/user"
+import { User } from '../api/types'
 
 const AllUsers = () => {
-    return null
+    const [users, setUsers] = useState<Array<User>>([])
+
+    async function _getAllUsers(){
+        console.log("useEffect");
+        const data = await getAllUser();
+        setUsers(data)
+    }
+
+    useEffect(() => {
+        _getAllUsers();
+    }, []);
+
+    function renderUsers(values: User){
+        return (
+            <div key={values.id}>
+                <h1>{values.name}</h1>
+                    <ul>
+                        <li>Pseudo : {values.username}</li>
+                        <li>Email: {values.email}</li>
+                    </ul>
+            </div>
+        )
+    }
+
+    return <ul className = "user-list">{users.map(renderUsers)}</ul>
 }
 
 export default AllUsers
